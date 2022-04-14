891554941ba513248996e219d4092f7493054ed2
Laetitia Hars <laetitia.hars@gmail.com>
2022-04-14 12:30:56 +0200

    :sparkles: add Footer component

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,3 +1,39 @@
-const Footer = () => {};
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import logo_white from "../../assets/logo_white.png";
+
+const FooterContainer = styled.div`
+  height: 209px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  background-color: #000000;
+  color: #ffffff;
+  justify-content: space-around;
+  align-items: center;
+  padding: 30px 0 30px;
+  box-sizing: border-box;
+`;
+
+const HomeLogoFooter = styled.img`
+  height: 40px;
+`;
+
+const FooterText = styled.div`
+  font-size: 12px;
+
+  @media (min-width: 1024px) {
+    font-size: 24px;
+  }
+`;
+
+const Footer = () => (
+  <FooterContainer>
+    <Link to="/">
+      <HomeLogoFooter src={logo_white} />
+    </Link>
+    <FooterText>© 2020 Kasa. All rights reserved</FooterText>
+  </FooterContainer>
+);
 
 export default Footer;
---
+++ src/utils/variables.js
@@ -0,0 +1,7 @@
+const height = {
+  footer: 209,
+  headerLarge: 192,
+  headerSmall: 91,
+};
+
+export default height;
