e70ace894219e419ce72c55808f8397d0ff93c62
Laetitia Hars <laetitia.hars@gmail.com>
2022-05-06 11:31:17 +0200

    :sparkles: update banner component to integrate radius props

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -15,26 +15,49 @@ const BannerContainer = styled.div`
 const BannerImg = styled.img`
   width: 100%;
   height: 100%;
-  border-radius: 25px;
+  border-radius: ${({ radius }) => radius};
   object-fit: cover;
+
+  @media (min-width: 1024px) {
+    border-radius: 25px;
+  }
 `;
 
 const Background = styled.div`
   background: #000000;
   mix-blend-mode: darken;
   opacity: 0.3;
-  border-radius: 25px;
-  height: 223px;
+  border-radius: ${({ radius }) => radius};
+  height: 100%;
   width: 100%;
   position: absolute;
   top: 0;
+
+  @media (min-width: 1024px) {
+    border-radius: 25px;
+  }
 `;
 
-const Banner = ({ source, height }) => {
+const Text = styled.div`
+  color: #ffffff;
+  position: absolute;
+  top: 30%;
+  font-size: 24px;
+  font-weight: 500;
+  margin-left: 16px;
+
+  @media (min-width: 1024px) {
+    font-size: 48px;
+    right: 32%;
+  }
+`;
+
+const Banner = ({ source, height, radius, text }) => {
   return (
     <BannerContainer height={height}>
-      <BannerImg src={source} />
-      <Background />
+      <BannerImg src={source} radius={radius} />
+      <Background radius={radius} />
+      {text && <Text>{text}</Text>}
     </BannerContainer>
   );
 };
@@ -44,8 +67,11 @@ export default Banner;
 Banner.propTypes = {
   source: PropTypes.string.isRequired,
   height: PropTypes.string,
+  radius: PropTypes.string,
+  text: PropTypes.string,
 };
 
 Banner.defaultProps = {
   height: "223px",
+  radius: "25px",
 };
